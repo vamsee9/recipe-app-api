a305d6eb57762a23ad2550439827b8df5553e869
vamsee9 <4n0n5ur7@protonmail.com>
2024-03-03 11:57:58 +0530

    added Database tests
    Signed-off-by: vamsee9 <4n0n5ur7@protonmail.com>

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -15,20 +15,20 @@ class CommandTest(SimpleTestCase):
     """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
-        """Test waiting for DB if DB is ready""" 
+        """Test waiting for DB if DB is ready"""
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for DB when getting OperationalError"""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError]  * 3 + [True]
-        
+            [OperationalError] * 3 + [True]
+
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
